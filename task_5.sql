d20830fa16ddf05758dc295a08484777f1e21198
ODOYO CORNELLIUS NYASWA <107390845+odoyocorneliusnyaswa@users.noreply.github.com>
2025-07-05 20:10:44 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,12 +1,2 @@
--- Script to insert a single customer record
-INSERT INTO Customers (
-    customer_id,
-    customer_name,
-    email,
-    address
-) VALUES (
-    1,
-    'Cole Baidoo',
-    'cbaidoo@sandtech.com',
-    '123 Happiness Ave.'
-);
+INSERT INTO CUSTOMER (CUSTOMER_ID, CUSTOMER_NAME, EMAIL, ADDRESS)
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
