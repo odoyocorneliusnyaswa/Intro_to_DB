e6f56a54409c62dd26950980f7ab9a8c93f5461e
ODOYO CORNELLIUS NYASWA <107390845+odoyocorneliusnyaswa@users.noreply.github.com>
2025-07-05 20:14:10 +0300

    Update task_4.sql

--- task_4.sql
+++ task_4.sql
@@ -1,12 +1,13 @@
+USE alx_book_store;
+
 SELECT 
-    COLUMN_NAME, 
-    COLUMN_TYPE, 
-    IS_NULLABLE, 
-    COLUMN_KEY, 
-    COLUMN_DEFAULT, 
-    EXTRA
+    COLUMN_NAME AS 'Column Name', 
+    DATA_TYPE AS 'Data Type', 
+    IS_NULLABLE AS 'Is Nullable', 
+    COLUMN_DEFAULT AS 'Default Value', 
+    CHARACTER_MAXIMUM_LENGTH AS 'Max Length'
 FROM 
     INFORMATION_SCHEMA.COLUMNS
 WHERE 
     TABLE_NAME = 'books' 
-    AND TABLE_SCHEMA = DATABASE();
+    AND TABLE_SCHEMA = 'alx_book_store';
