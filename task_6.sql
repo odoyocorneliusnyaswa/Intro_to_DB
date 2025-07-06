f70df5ce031f5faee118bcc7aaf1ef1b093049d8
ODOYO CORNELLIUS NYASWA <107390845+odoyocorneliusnyaswa@users.noreply.github.com>
2025-07-06 11:34:39 +0300

    Update task_6.sql

--- task_6.sql
+++ task_6.sql
@@ -1,7 +1,9 @@
+-- Script to insert multiple customer records into alx_book_store database
 USE alx_book_store;
 
--- Script to insert multiple customer records
-INSERT INTO Customers (customer_id, customer_name, email, address) VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+-- Insert multiple rows into customer table in a single statement
+INSERT INTO customer (customer_id, customer_name, email, address)
+VALUES 
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
